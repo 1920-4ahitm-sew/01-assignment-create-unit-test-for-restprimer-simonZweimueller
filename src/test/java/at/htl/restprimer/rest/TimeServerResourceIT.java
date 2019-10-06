30a7ee330ecd45e0ed348d93858cbca8e560f881
Simon Zweimueller <simon.zweimueller1@gmail.com>
2019-10-06 13:15:13 +0200

    wrote test case

--- src/test/java/at/htl/restprimer/rest/TimeServerResourceIT.java
+++ src/test/java/at/htl/restprimer/rest/TimeServerResourceIT.java
@@ -1,4 +1,34 @@
 package at.htl.restprimer.rest;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 public class TimeServerResourceIT {
+
+    private Client client;
+    private WebTarget target;
+
+    @BeforeEach
+    public void initClient() {
+        this.client = ClientBuilder.newClient();
+        this.target = client.target("http://localhost:8080/restprimer/api/time");
+    }
+
+    @Test
+    public void fetchVehicle() {
+        Response response = this.target.request(MediaType.TEXT_PLAIN).get();
+        assertThat(response.getStatus(), is(200));
+        String payload = response.readEntity(String.class);
+        assertTrue(payload.startsWith("Time: "));
+    }
 }
